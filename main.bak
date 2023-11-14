99296f86a8a301294039aefd8d95ef1bc32681de
thq642 <mikail.thoriq@gmail.com>
2023-11-14 18:52:25 +0700

    NEW!! with GUI

---
+++ main.bak
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"sync"
+	"strings"
+)
+
+const confereceTikets = 50
+
+var confereceName = "concert"
+var remainingTickets uint = 50
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var bookings = make([]UserData, 0)
+
+var wg = sync.WaitGroup{}
+
+func validateUser(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@") && len(email) >= 2
+	isValidTicketAmount := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketAmount
+}
+
+func main() {
+	greetUsers()
+
+	firstName, lastName, email, userTickets := userInput()
+	isValidName, isValidEmail, isValidTicketAmount := validateUser(firstName, lastName, email, userTickets)
+
+	if isValidName && isValidEmail && isValidTicketAmount {
+		bookTickets(userTickets, firstName, lastName, email)
+		wg.Add(1)
+		go sendTickets(firstName, lastName, userTickets, email)
+
+		fmt.Printf("Firstnames of bookings are %v\n", getFirstNames())
+		noTicketsCondition := remainingTickets == 0
+
+		if noTicketsCondition {
+			fmt.Println("All tickets was reserved")
+		}
+	} else {
+		if !isValidName {
+			fmt.Println("First name or last name is too short")
+		}
+		if !isValidEmail {
+			fmt.Println("Email address is not valid")
+		}
+		if !isValidTicketAmount {
+			fmt.Println("Number of tickets that you entered is not valid")
+		}
+	}
+	wg.Wait()
+}
+
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", confereceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confereceTikets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+func getFirstNames() []string {
+	firstNames := []string{}
+
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+
+	return firstNames
+}
+
+func userInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter how many tikets you wanna book: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTickets(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+
+	userData := UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confereceName)
+}
+
+func sendTickets(firstName string, lastName string, userTickets uint, email string) {
+	time.Sleep(10 * time.Second)
+	tickets := fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("##################")
+	fmt.Printf("Sending ticket: \n %v \n to email address %v\n", tickets, email)
+	fmt.Println("##################")
+	wg.Done()
+}
--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"sync"
 	"strings"
+	"strconv"
+	"time"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
-const confereceTikets = 50
+const conferenceTickets = 50
 
-var confereceName = "concert"
+var conferenceName = "concert"
 var remainingTickets uint = 50
 
 type UserData struct {
@@ -21,7 +27,8 @@ type UserData struct {
 
 var bookings = make([]UserData, 0)
 
-var wg = sync.WaitGroup{}
+var myApp fyne.App
+var myWindow fyne.Window
 
 func validateUser(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
@@ -32,70 +39,89 @@ func validateUser(firstName string, lastName string, email string, userTickets u
 }
 
 func main() {
-	greetUsers()
+	myApp = app.New()
+	myWindow = myApp.NewWindow("Conference Booking")
 
-	firstName, lastName, email, userTickets := userInput()
-	isValidName, isValidEmail, isValidTicketAmount := validateUser(firstName, lastName, email, userTickets)
+	tabs := container.NewAppTabs(
+		tabItem("Book Tickets", bookingTab()),
+		tabItem("Booking List", bookingListTab()),
+	)
 
-	if isValidName && isValidEmail && isValidTicketAmount {
-		bookTickets(userTickets, firstName, lastName, email)
-		wg.Add(1)
-		go sendTickets(firstName, lastName, userTickets, email)
+	myWindow.SetContent(tabs)
+	myWindow.ShowAndRun()
+}
 
-		fmt.Printf("Firstnames of bookings are %v\n", getFirstNames())
-		noTicketsCondition := remainingTickets == 0
+func bookingTab() fyne.CanvasObject {
+	firstNameEntry := widget.NewEntry()
+	lastNameEntry := widget.NewEntry()
+	emailEntry := widget.NewEntry()
+	ticketsEntry := widget.NewEntry()
 
-		if noTicketsCondition {
-			fmt.Println("All tickets was reserved")
-		}
-	} else {
-		if !isValidName {
-			fmt.Println("First name or last name is too short")
-		}
-		if !isValidEmail {
-			fmt.Println("Email address is not valid")
-		}
-		if !isValidTicketAmount {
-			fmt.Println("Number of tickets that you entered is not valid")
-		}
+	form := &widget.Form{
+		OnSubmit: func() {
+			firstName := firstNameEntry.Text
+			lastName := lastNameEntry.Text
+			email := emailEntry.Text
+			ticketsStr := ticketsEntry.Text
+
+			userTickets, err := parseTicketInput(ticketsStr)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			isValidName, isValidEmail, isValidTicketAmount := validateUser(firstName, lastName, email, userTickets)
+
+			if isValidName && isValidEmail && isValidTicketAmount {
+				bookTickets(userTickets, firstName, lastName, email)
+				sendTicketsAsync(firstName, lastName, userTickets, email)
+				dialog.ShowInformation("Booking Success", "Your tickets have been booked successfully.", myWindow)
+			} else {
+				var errorMsg string
+				if !isValidName {
+					errorMsg += "First name or last name is too short\n"
+				}
+				if !isValidEmail {
+					errorMsg += "Email address is not valid\n"
+				}
+				if !isValidTicketAmount {
+					errorMsg += "Number of tickets that you entered is not valid\n"
+				}
+				dialog.ShowError(fmt.Errorf(errorMsg), myWindow)
+			}
+		},
 	}
-	wg.Wait()
+
+	form.Append("First Name", firstNameEntry)
+	form.Append("Last Name", lastNameEntry)
+	form.Append("Email", emailEntry)
+	form.Append("Number of Tickets", ticketsEntry)
+
+	return container.New(layout.NewCenterLayout(), form)
 }
 
-func greetUsers() {
-	fmt.Printf("Welcome to %v booking application\n", confereceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", confereceTikets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-}
-func getFirstNames() []string {
-	firstNames := []string{}
+func bookingListTab() fyne.CanvasObject {
+	list := widget.NewList(
+		func() int {
+			return len(bookings)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*widget.Label).SetText(fmt.Sprintf("%s %s: %d tickets", bookings[id].firstName, bookings[id].lastName, bookings[id].numberOfTickets))
+		},
+	)
 
-	for _, booking := range bookings {
-		firstNames = append(firstNames, booking.firstName)
+	return container.New(layout.NewCenterLayout(), list)
+}
+
+func parseTicketInput(input string) (uint, error) {
+	tickets, err := strconv.ParseUint(input, 10, 32)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse tickets: %v", err)
 	}
-
-	return firstNames
-}
-
-func userInput() (string, string, string, uint) {
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-
-	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&firstName)
-
-	fmt.Println("Please enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Please enter your email address: ")
-	fmt.Scan(&email)
-
-	fmt.Println("Please enter how many tikets you wanna book: ")
-	fmt.Scan(&userTickets)
-
-	return firstName, lastName, email, userTickets
+	return uint(tickets), nil
 }
 
 func bookTickets(userTickets uint, firstName string, lastName string, email string) {
@@ -110,15 +136,19 @@ func bookTickets(userTickets uint, firstName string, lastName string, email stri
 
 	bookings = append(bookings, userData)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets.You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confereceName)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
 
-func sendTickets(firstName string, lastName string, userTickets uint, email string) {
-	time.Sleep(10 * time.Second)
-	tickets := fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
-	fmt.Println("##################")
-	fmt.Printf("Sending ticket: \n %v \n to email address %v\n", tickets, email)
-	fmt.Println("##################")
-	wg.Done()
+func sendTicketsAsync(firstName string, lastName string, userTickets uint, email string) {
+	go func() {
+		time.Sleep(10 * time.Second)
+		tickets := fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+		// Consider adding error handling here
+		fmt.Printf("Sending ticket: \n %v \n to email address %v\n", tickets, email)
+	}()
+}
+
+func tabItem(name string, content fyne.CanvasObject) *container.TabItem {
+	return container.NewTabItem(name, content)
 }
